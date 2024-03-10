3e3ce6b350318cc7cb6268fbf1a9eecfe663bf92
Fabián Huertas <miloh9318@hotmail.com>
2024-03-09 19:08:34 -0500

    añadiendo un spinner

--- js/app.js
+++ js/app.js
@@ -12,11 +12,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]')
     const btnReset = document.querySelector('#formulario button[type="reset"]')
+    const spinner = document.querySelector('#spinner');
 
     //Asignar Eventos
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
+    formulario.addEventListener('submit', enviarEmail);
     btnReset.addEventListener('click', function(e) {
         e.preventDefault();
 
@@ -28,6 +30,13 @@ document.addEventListener('DOMContentLoaded', function () {
         formulario.reset();
         comprobarEmail();
     })
+
+    function enviarEmail(e) {
+        e.preventDefault();
+
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+    }
 
     function validar(e) {
         if(e.target.value.trim() === '') {
